c4f7070fd22b86265bfd76362ac885bf9ab960de
Luis Pedro Coelho <luis@luispedro.org>
2016-05-02 18:12:08 +0200

    ENH Use nix to get Rpy to just work

---
+++ default.nix
@@ -0,0 +1,31 @@
+let
+  pkgs = import <nixpkgs> {};
+in
+{ stdenv ? pkgs.stdenv, python ? pkgs.python27, pythonPackages ? pkgs.python27Packages }:
+
+let
+  jug = import ../jug { inherit python pythonPackages ; };
+in
+  stdenv.mkDerivation {
+     name = "partialUenv";
+     version = "0.0.0";
+     src = ./.;
+     propagatedBuildInputs = with pythonPackages; [
+            pkgs.readline6
+            pkgs.python
+            pkgs.liblapack
+            pkgs.R
+            pkgs.rPackages.npsm
+            sqlite3
+            rpy2
+            ipython
+            numpy
+            scipy
+            virtualenv
+            jug
+            pkgs.zsh
+            ];
+}
+
+
+
