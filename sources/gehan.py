25573ce22529e6c0843859d333008fde7024b03c
Luis Pedro Coelho <luis@luispedro.org>
2016-05-10 16:09:10 +0200

    BUG Finally really fix Gehan test

--- sources/gehan.py
+++ sources/gehan.py
@@ -9,18 +9,18 @@ def gehan2sided(va, vb, ra, rb, la, lb):
     v = np.concatenate( [va, vb] )
     r = np.concatenate( [ra, rb] )
     l = np.concatenate( [la, lb] )
+    ok = ~r | ~l
 
     oand = np.logical_and.outer
     e = ~r & ~l
     s = np.sign( np.subtract.outer(v, v) )
-    s *= (oand(e,e)
-            | (oand(e, r)&(s > 0))  | (oand(r, e)&(s < 0))
-            | (oand(e, l) & (s < 0)) | (oand(l, e)&(s > 0))
-            | (oand(r, l) & (s > 0)) | (oand(l, r)&(s < 0)))
+    s *= oand(ok,ok) & (
+            (oand(e,e)
+            | (oand(e, r) & (s < 0)) | (oand(r, e) & (s > 0))
+            | (oand(e, l) & (s > 0)) | (oand(l, e) & (s < 0))
+            | (oand(r, l) & (s > 0)) | (oand(l, r) & (s < 0))))
     w = s.sum(1)
-    labels = np.ones(n1+n2, bool)
-    labels[n1:] = False
-    stat = w[labels].sum()
+    stat = w[:n1].sum()
     p = stats.norm.cdf(stat/np.sqrt(n1*n2/(n1+n2+1)*w.var()))
     p = min(p, 1-p)
     p *= 2
