75c7c6c441274725999f4cd675c3e06f1ae01bdd
Luis Pedro Coelho <luis@luispedro.org>
2016-05-10 16:09:34 +0200

    ENH Use the statistic from Gehans test

--- sources/partialU.py
+++ sources/partialU.py
@@ -11,23 +11,23 @@ class Diff(object):
             self.type = 'gt'
             self.value = e - (-7)
         else:
-            self.type = 'gt'
+            self.type = 'lt'
             self.value = (-7) - s
     def __str__(self):
         if self.type == 'NA': return self.type
         return '{}({})'.format(self.type, self.value)
     __repr__ = __str__
 
-#=======+=======+========+=======+=========================
-#       | exact | exact  | lt y  | lt y   |  gt y |   gt y
-#       | x < y | x >= y | x < y | x >= y | x < y | x >= y
-#=======+=======+========+=======+=========================
-# exact | True  | False  |  NA   |  False | True  | NA
-#-------+-------+--------+-------+--------+-------+--------
-#  lt x | True  | NA     |  NA   |  NA    | True  | NA
-#-------+-------+--------+-------+--------+-------+--------
-#  gt x | NA    | False  |  NA   |  False | NA    | NA
-#-------+-------+--------+-------+--------+-------+--------
+#=======+=========+==========+=======+=========================
+#       | exact y | exact  y | lt y  | lt y   |  gt y |   gt y
+#       |  x < y  |  x >= y  | x < y | x >= y | x < y | x >= y
+#=======+=========+==========+=======+=========================
+# exact | True    | False    |  NA   |  False | True  | NA
+#-------+---------+----------+-------+--------+-------+--------
+#  lt x | True    | NA       |  NA   |  NA    | True  | NA
+#-------+---------+----------+-------+--------+-------+--------
+#  gt x | NA      | False    |  NA   |  False | NA    | NA
+#-------+---------+----------+-------+--------+-------+--------
 
 table = {
         ('exact', 'exact'): (True, False),
@@ -53,15 +53,11 @@ def partial_U(values0, values1, cmp=None):
     if cmp is None:
         cmp = lt
     smaller = 0.
-    valid = 0
     for d0 in values0:
         for d1 in values1:
             c = cmp(d0, d1)
             if c != 'NA':
-                valid += 1
                 smaller += (-1 if c else +1)
-    if valid == 0:
-        return 1.
     return smaller
 
 def partial_U_test(values0, values1, n_permutations=10000):
@@ -70,18 +66,18 @@ def partial_U_test(values0, values1, n_permutations=10000):
     diffs = values0 + values1
     diff_ids = list(range(len(diffs)))
 
-    table_valid = np.zeros((len(diffs), len(diffs)))
-    table_smaller = np.zeros((len(diffs), len(diffs)))
+    table_U = np.zeros((len(diffs), len(diffs)))
     for i,d0 in enumerate(diffs):
         for j, d1 in enumerate(diffs):
+            if i == j: continue
             v = lt(d0,d1)
             if v != 'NA':
-                table_valid[i,j] = 1
-                table_smaller[i,j] = v
+                table_U[i,j] = (-1 if v else +1)
 
     better = 0
     for _ in range(n_permutations):
         random.shuffle(diff_ids)
-        cur = table_smaller[diff_ids[:N0]].T[diff_ids[N0:]].sum()/table_valid[diff_ids[:N0]].T[diff_ids[N0:]].sum()
-        better += (np.abs(cur) < np.abs(base))
+        cur = table_U[diff_ids[:N0]].T[diff_ids[N0:]].sum()
+        better += (np.abs(cur) > np.abs(base))
     return base, float(better + 1)/n_permutations
+
